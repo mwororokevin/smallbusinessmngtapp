236d21e6a03132ea21c397aa6282b2ac448c4e3c
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2023-09-20 13:46:00 +0300

    Add the Passay dependancy.

---
+++ SmallBusinessMngtAPI/src/main/java/com/mwororokevin/smallbusinessmanagement/util/GenerateUniqueUsername.java
@@ -0,0 +1,2 @@
+package com.mwororokevin.smallbusinessmanagement.util;public class GenerateUniqueUsername {
+}
---
+++ SmallBusinessMngtAPI/src/main/java/com/mwororokevin/smallbusinessmanagement/util/PasswordPassayGenerator.java
@@ -0,0 +1,37 @@
+package com.mwororokevin.smallbusinessmanagement.util;
+
+import org.passay.CharacterData;
+import org.passay.CharacterRule;
+import org.passay.EnglishCharacterData;
+import org.passay.PasswordGenerator;
+
+public class PasswordGenerator {
+    public static String generatePassayPassword() {
+        PasswordGenerator passwordGenerator = new PasswordGenerator();
+        CharacterData lowerCaseChars = EnglishCharacterData.LowerCase;
+        CharacterRule lowerCaseRule = new CharacterRule(lowerCaseChars);
+        lowerCaseRule.setNumberOfCharacters(3);
+
+        CharacterData upperCaseChars = EnglishCharacterData.UpperCase;
+        CharacterRule upperCaseRule = new CharacterRule(upperCaseChars);
+        upperCaseRule.setNumberOfCharacters(3);
+
+        CharacterData digitChars = EnglishCharacterData.Digit;
+        CharacterRule digitRule = new CharacterRule(digitChars);
+        digitRule.setNumberOfCharacters(3);
+
+        CharacterData specialChars = new CharacterData() {
+            public String getErrorCode() {
+                return ERROR_CODE;
+            }
+
+            public String getCharacters() {
+                return "!@#$%^&*()_+'";
+            }
+        };
+        CharacterRule splitCharRule = new CharacterRule(specialChars);
+        splitCharRule.setNumberOfCharacters(2);
+
+        return passwordGenerator.generatePassword(12, splitCharRule, lowerCaseRule, upperCaseRule, digitRule);
+    }
+}
