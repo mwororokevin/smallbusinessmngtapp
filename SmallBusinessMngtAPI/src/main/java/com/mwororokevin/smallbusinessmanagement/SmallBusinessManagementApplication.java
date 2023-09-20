07b50a92b8925dc0a025c5e8d19d0704075dd851
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2023-09-20 10:56:05 +0300

    Make initial commit.

---
+++ SmallBusinessMngtAPI/src/main/java/com/mwororokevin/smallbusinessmanagement/SmallBusinessManagementApplication.java
@@ -0,0 +1,13 @@
+package com.mwororokevin.smallbusinessmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallBusinessManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallBusinessManagementApplication.class, args);
+	}
+
+}
---
+++ SmallBusinessMngtAPI/src/test/java/com/mwororokevin/smallbusinessmanagement/SmallBusinessManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwororokevin.smallbusinessmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallBusinessManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
