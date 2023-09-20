2f0a0818a0bbcbef4d25ab548f93ec194a742f47
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2023-09-20 11:33:43 +0300

    Add the Users Model.

--- SmallBusinessMngtAPI/src/main/java/com/mwororokevin/smallbusinessmanagement/model/Users.java
+++ SmallBusinessMngtAPI/src/main/java/com/mwororokevin/smallbusinessmanagement/model/Users.java
@@ -1,2 +1,46 @@
-package com.mwororokevin.smallbusinessmanagement.model;public class Users {
+package com.mwororokevin.smallbusinessmanagement.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @NotBlank(message = "Please add Surname.")
+    private String surname;
+
+    @NotBlank(message = "Please add other names.")
+    @Column(name = "other_names")
+    private String otherNames;
+
+    private String username;
+
+    @Length(min = 10)
+    private String password;
+
+    @Email(message = "Incorrect Email Address format.")
+    @Column(name = "user_email")
+    private String email;
+
+    @Column(name = "creation_date_time")
+    private LocalDateTime creationDateTime;
+
+    @Column(name = "update_date_time")
+    private LocalDateTime updateDateTime;
 }
