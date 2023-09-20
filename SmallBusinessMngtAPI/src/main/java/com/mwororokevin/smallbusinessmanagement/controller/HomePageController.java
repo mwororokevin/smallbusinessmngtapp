7b34a192a3d639afff66e9db8dab8399588fafae
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2023-09-20 11:15:49 +0300

    Add the Homepage Controller for testing and change the server port in the application properties file.

--- SmallBusinessMngtAPI/src/main/java/com/mwororokevin/smallbusinessmanagement/controller/HomePageController.java
+++ SmallBusinessMngtAPI/src/main/java/com/mwororokevin/smallbusinessmanagement/controller/HomePageController.java
@@ -1,2 +1,15 @@
-package com.mwororokevin.smallbusinessmanagement.controller;public class HomePageController {
+package com.mwororokevin.smallbusinessmanagement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomePageController {
+//    @GetMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String sayHello() {
+        return "Hello Spring Boot User";
+    }
 }
