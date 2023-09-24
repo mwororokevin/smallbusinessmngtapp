3e2cb380389ea7b1b042f4e1bbbbee4d73623569
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2023-09-24 18:50:43 +0300

    Change the label and input type from email to username and text respectively.

--- SmallBusinessMngtFrontEnd/src/components/Login.tsx
+++ SmallBusinessMngtFrontEnd/src/components/Login.tsx
@@ -3,26 +3,26 @@ import React from 'react';
 export default function Login() {
   return (
     <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
-      <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
+      <div className="w-full p-6 m-auto bg-white rounded-md shadow-2xl lg:max-w-xl">
         <h1 className="text-3xl font-semibold text-center text-blue-700 underline">
           Sign in
         </h1>
         <form className="mt-6">
           <div className="mb-2">
             <label
-              for="email"
+              htmlFor="username"
               className="block text-sm font-semibold text-gray-800"
             >
-              Email/Username
+              Username
             </label>
             <input
-              type="email"
+              type="text"
               className="block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
           </div>
           <div className="mb-2">
             <label
-              for="password"
+              htmlFor="password"
               className="block text-sm font-semibold text-gray-800"
             >
               Password
