c1eb01e5699b832d6fde769b357dd9829973407e
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2023-09-24 21:25:52 +0300

    Add the Navbar component.

---
+++ SmallBusinessMngtFrontEnd/src/components/Users.tsx
@@ -0,0 +1,13 @@
+
+export default function Users() {
+  return (
+    <div className="">
+      <div className="">
+
+      </div>
+      <div className="flex flex-row">
+
+      </div>
+    </div>
+  );
+}
