fa0795027d2b26a6e8be01690267a3b98555ebe2
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2023-09-24 15:17:47 +0300

    Add the SupplierController methods to the SuppliersService class.

--- SmallBusinessMngtAPI/src/main/java/com/mwororokevin/smallbusinessmanagement/Suppliers/SuppliersService.java
+++ SmallBusinessMngtAPI/src/main/java/com/mwororokevin/smallbusinessmanagement/Suppliers/SuppliersService.java
@@ -1,4 +1,17 @@
 package com.mwororokevin.smallbusinessmanagement.Suppliers;
 
+import java.util.List;
+
 public interface SuppliersService {
+    Suppliers saveNewSupplier(Suppliers suppliers);
+
+    List<Suppliers> getAllSuppliers();
+
+    Suppliers getSupplierById(Long supplierId);
+
+    void deleteSupplierById(Long supplierId);
+
+    Suppliers updateSupplier(Long supplierId, Suppliers suppliers);
+
+    Suppliers fetchSupplierByNameIgnoreCase(String supplierName);
 }
