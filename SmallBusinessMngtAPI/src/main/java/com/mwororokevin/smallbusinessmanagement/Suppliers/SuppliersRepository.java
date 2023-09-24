23c6eedba3186295638afb70a4e339910ee2e4f5
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2023-09-24 16:32:04 +0300

    Add the findBySupplierName method.

--- SmallBusinessMngtAPI/src/main/java/com/mwororokevin/smallbusinessmanagement/Suppliers/SuppliersRepository.java
+++ SmallBusinessMngtAPI/src/main/java/com/mwororokevin/smallbusinessmanagement/Suppliers/SuppliersRepository.java
@@ -1,7 +1,9 @@
 package com.mwororokevin.smallbusinessmanagement.Suppliers;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SuppliersRepository {
+public interface SuppliersRepository extends JpaRepository<Suppliers, Long> {
+    Suppliers findBySupplierName(String supplierName);
 }
